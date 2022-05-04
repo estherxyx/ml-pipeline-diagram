26bf3f26839fdf592680df17fdd1e5f786e3481d
EstherXu <96934319+estherxyx@users.noreply.github.com>
2022-05-03 23:37:50 -0700

    Add diagram src

---
+++ src/diagram.py
@@ -0,0 +1,60 @@
+from diagrams import Diagram, Cluster
+from diagrams.aws.compute import Lambda
+from diagrams.aws.storage import S3
+from diagrams.aws.compute import Fargate
+from diagrams.aws.general import User
+from diagrams.aws.iot import IotCar
+
+node_attr = {
+    "fontsize": "8"
+}
+
+graph_attr = {
+    "bgcolor": "transparent"
+}
+
+with Diagram(
+        name="Application Cross Validation Workflow",
+        outformat="svg",
+        filename="output",
+        show=False,
+        node_attr=node_attr,
+        graph_attr=graph_attr):
+    input = User("RS/PM")
+    with Cluster("Data Processing Flow"):
+        dp_entry = S3("Raw Data Input")
+
+        with Cluster("Applications"):
+            software = [
+                Fargate("App-A"),
+                Fargate("App-B"),
+                Fargate("App-C")
+            ]
+
+        with Cluster("Outputs"):
+            dp_exit = [
+                S3(app.label+"-Output") for app in software
+            ]
+
+        dp_entry >> Lambda("Input Normalization Handler") >> software
+        for i in range(len(software)):
+            software[i] >> dp_exit[i]
+
+
+    with Cluster("Output Normalization Flow"):
+        mi_entry = Lambda("Output Normalization Handler")
+        mi_exit = S3("Normalized Output")
+        mi_entry >> mi_exit
+
+    with Cluster("Ground Truth Flow"):
+        vf_entry = IotCar("IoT")
+        vf_exit = S3("Real World Output")
+        vf_entry >> vf_exit
+
+    output = User("RS")
+
+    input >> dp_entry
+    dp_exit >> mi_entry
+    input >> vf_entry
+    mi_exit >> output
+    vf_exit >> output
